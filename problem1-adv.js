980128a397eaa9934fffc98a36bbe1ac36e306ea
marcos7530 <88636203+marcos7530@users.noreply.github.com>
2023-07-27 19:57:35 -0300

    se cambiaron variables que estaban en español en el punto 1 a ingles

--- problem1-adv.js
+++ problem1-adv.js
@@ -2,19 +2,17 @@ let num = 603;
 
 console.log(multipleOfThree(num));
 
-function multipleOfThree(numero) {
+function multipleOfThree(number) {
+  let sum = 0;
+  let digitos = number.toString().split(""); //hago un arreglo de los digitos ingresados
 
-  let suma = 0;
-  let digitos = numero.toString().split("");//hago un arreglo de los digitos ingresados
-
-  for (let i = 0; i < digitos.length; i++) {
-    suma += parseInt(digitos[i]);//recorro el arreglo y sumo sus elementos
+  for (let i = 0; i < digits.length; i++) {
+    sum += parseInt(digits[i]); //recorro el arreglo y sumo sus elementos
   }
 
-  if (suma > 9) {
-    multipleOfThree(suma);//funcion recursiva x si el numero ingresado tiene mas de 1 digito 
+  if (sum > 9) {
+    multipleOfThree(sum); //funcion recursiva x si el numero ingresado tiene mas de 1 digito
   } else {
-    return suma === 3 || suma === 6 || suma === 9;//condicion para que el numero sea multiplo de 3
+    return sum === 3 || sum === 6 || sum === 9; //condicion para que el numero sea multiplo de 3
   }
 }
-
