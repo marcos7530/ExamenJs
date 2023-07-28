c78eced1d95fab705e79c60a328edd2db323d8db
marcos7530 <88636203+marcos7530@users.noreply.github.com>
2023-07-27 21:29:18 -0300

    arregle los comentarios en el problema 1

--- problem1-adv.js
+++ problem1-adv.js
@@ -4,10 +4,13 @@ console.log(multipleOfThree(num));
 
 function multipleOfThree(number) {
   let sum = 0;
-  let digitos = number.toString().split(""); //hago un arreglo de los digitos ingresados
 
+  //hago un arreglo de los digitos ingresados
+  let digits = number.toString().split("");
+
+  //recorro el arreglo y sumo sus elementos
   for (let i = 0; i < digits.length; i++) {
-    sum += parseInt(digits[i]); //recorro el arreglo y sumo sus elementos
+    sum += parseInt(digits[i]);
   }
 
   if (sum > 9) {
