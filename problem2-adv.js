9c5c1906b37bf78bb7e50f3335768db57c255ac4
marcos7530 <88636203+marcos7530@users.noreply.github.com>
2023-07-27 21:27:06 -0300

    acomode los comentarios en el problema 2

--- problem2-adv.js
+++ problem2-adv.js
@@ -7,15 +7,20 @@ function timeFromTheThird() {
   let differenceBetweenDates =
     actualDate.getTime() - ARGENTINA_CHAMPION_DATE.getTime();
 
-  let days = Math.floor(differenceBetweenDates / (1000 * 60 * 60 * 24)); //numero de milisegundos en dias, el metodo floor redondea hacia abajo
+  //numero de milisegundos en dias, el metodo floor redondea hacia abajo
+  let days = Math.floor(differenceBetweenDates / (1000 * 60 * 60 * 24));
 
-  differenceBetweenDates -= days * (1000 * 60 * 60 * 24); // a la diferencia le resto los milisegundos de los dias
+  // a la diferencia le resto los milisegundos de los dias
+  differenceBetweenDates -= days * (1000 * 60 * 60 * 24);
 
-  let hours = Math.floor(differenceBetweenDates / (1000 * 60 * 60)); //numero de milisegundos en  horas
+  //numero de milisegundos en  horas
+  let hours = Math.floor(differenceBetweenDates / (1000 * 60 * 60));
 
-  differenceBetweenDates -= hours * (1000 * 60 * 60); // a la diferencia le resto los milisegundos de lashoras
+  // a la diferencia le resto los milisegundos de las horas
+  differenceBetweenDates -= hours * (1000 * 60 * 60);
 
-  let minutes = Math.floor(differenceBetweenDates / (1000 * 60)); //numero de milisegundos en minutos
+  //numero de milisegundos en minutos
+  let minutes = Math.floor(differenceBetweenDates / (1000 * 60));
 
   console.log(
     `Pasaron ${days} días, ${hours} horas y ${minutes} minutos desde el Domingo, 18 de diciembre de 2022`
